bfa83f7176f11413df1d1e0d1901888dcb725934
Ho Sy Phat <65226220+R4ptorVN@users.noreply.github.com>
2024-02-27 21:32:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-console.log("JS TEXT");
-console.log("i love plinh");
+const change1btn = document.getElementById("change1");
+
+let statuschange1 = 0;
+change1btn.onclick = function()
+{
+    statuschange1++;
+    if(statuschange1 == 51)
+    {
+        document.getElementById("Po").textContent =  `Oh no! Po is out of breathe!`;
+        document.getElementById("change1").textContent = `Feed him some water!`;
+    }
+    else if(statuschange1 > 51)
+    {
+        statuschange1 = 0;
+        document.getElementById("Po").textContent = `Woof Woof! x ${statuschange1}`;
+        document.getElementById("change1").textContent = `Bark!`;
+    }
+    else
+    {
+        document.getElementById("Po").textContent = `Woof Woof! x ${statuschange1}`;
+    }
+}
+
+
+
+
